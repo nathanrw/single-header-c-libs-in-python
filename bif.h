0b992c3c679d35c172ac585323fa0edd22cc20f0
Nathan Woodward <nathanrichardwoodward@gmail.com>
2018-12-01 09:27:17 +0000

    Initial implementation of demo + c lib

---
+++ bif.h
@@ -0,0 +1,130 @@
+/**
+ * Basic Image Format
+ **/
+ 
+#ifdef WIN32
+#  ifdef BIF_IMPLEMENTATION
+#    define BIF_API __declspec(dllexport)
+#  else
+#    define BIF_API __declspec(dllimport)
+#  endif
+#endif
+
+#ifdef __cplusplus
+  #define BIF_API_BEGIN extern "C" {
+  #define BIF_API_END }
+#else
+  #define BIF_API_BEGIN
+  #define BIF_API_END
+#endif
+
+BIF_API_BEGIN
+
+enum bif_error {
+    BIF_OK = 0,
+    BIF_ERROR_UNKNOWN,
+    BIF_ERROR_HORRIBLY_MANGLED
+};
+
+#define BIF_FLAG(N) (1 << N)
+
+#define BIF_CHECK_FLAG(VAR, FLAG) (!!(VAR & FLAG))
+
+enum bif_flags {
+    BIF_FLAG_NONE = 0,
+    BIF_FLAG_WIBBLE = BIF_FLAG(0),
+    BIF_FLAG_WOBBLE = BIF_FLAG(1)
+};
+
+struct bif_image {
+    uint16_t width;
+    uint16_t height;
+    uint32_t* buffer; // RGBA pixels.
+};
+
+BIF_API bif_error bif_image_read(const char* filename, bif_flags flags, bif_image* image);
+
+BIF_API bif_error bif_image_free(bif_image* image);
+
+BIF_API bif_error bif_image_write(const char* filename, bif_flags flags, bif_image* image);
+
+BIF_API_END
+ 
+#ifdef BIF_IMPLEMENTATION
+
+bif_error bif_image_read(const char* filename, bif_flags flags, bif_image* image)
+{
+    if (!filename || !*filename || !image) {
+        return BIF_ERROR_HORRIBLY_MANGLED;
+    }
+    if (!BIF_CHECK_FLAG(flags, BIF_FLAG_WIBBLE)) {
+        return BIF_ERROR_HORRIBLY_MANGLED;
+    }
+    FILE file = fopen(filename, "rb");
+    if (!file) {
+        return BIF_ERROR_HORRIBLY_MANGLED;
+    }
+    uint8_t bif[4];
+    if (fread(bif, 1, 4, file) != 4) {
+        return BIF_ERROR_HORRIBLY_MANGLED;
+    }
+    if (bif[0] != 'B' || bif[1] != 'I' || bif[2] != 'F' || bif[3] != '\0') {
+        return BIF_ERROR_HORRIBLY_MANGLED;
+    }
+    if (fread(&image->width, 2, 1, file) != 1) {
+        return BIF_ERROR_HORRIBLY_MANGLED;
+    }
+    if (fread(&image->height, 2, 1, file) != 1) {
+        return BIF_ERROR_HORRIBLY_MANGLED;
+    }
+    size_t pixel_count = image->width*image->height;
+    image->buffer = malloc(pixel_count*4);
+    if (!image->buffer) {
+        return BIF_ERROR_HORRIBLY_MANGLED;
+    }
+    if (fread(image->buffer, pixel_count, 4, file) != pixel_count) {
+        free(image->buffer);
+        return BIF_ERROR_HORRIBLY_MANGLED;
+    }
+    return BIF_OK;
+}
+
+bif_error bif_image_free(bif_image* image)
+{
+    free(image->buffer);
+    return BIF_OK;
+}
+
+bif_error bif_image_write(const char* filename, bif_flags flags, bif_image* image)
+{
+    if (!filename || !*filename || !image || !image->buffer) {
+        return BIF_ERROR_HORRIBLY_MANGLED;
+    }
+    if (!BIF_CHECK_FLAG(flags, BIF_FLAG_WOBBLE)) {
+        return BIF_ERROR_HORRIBLY_MANGLED;
+    }
+    size_t pixel_count = image->width*image->height;
+    if (pixel_count < 1) {
+        return BIF_ERROR_HORRIBLY_MANGLED;
+    }
+    FILE file = fopen(filename, 'wb');
+    if (!file) {
+        return BIF_ERROR_HORRIBLY_MANGLED;
+    }
+    uint8_t bif[] = {'B', 'I', 'F', '\0'};
+    if (fwrite(bif, 1, 4, file) != 4) {
+        return BIF_ERROR_HORRIBLY_MANGLED;
+    }
+    if (fwrite(&image->width, 2, 1, file) != 1) {
+        return BIF_ERROR_HORRIBLY_MANGLED;
+    }
+    if (fwrite(&image->height, 2, 1, file) != 1) {
+        return BIF_ERROR_HORRIBLY_MANGLED;
+    }
+    if (fwrite(image->buffer, pixel_count, 4, file) != pixel_count) {
+        return BIF_ERROR_HORRIBLY_MANGLED;
+    }
+    return BIF_OK;
+}
+
+#endif
---
+++ presentation.md
@@ -0,0 +1,12 @@
+# Easily re-use single header c libraries in Python using `cffi`
+
+## Nathan Woodward
+
+---
+
+Image
+
+    Artist	Unknown
+    Title	A Pair of Pigs
+    Date	 About 1850
+    Medium	Oil on canvas
